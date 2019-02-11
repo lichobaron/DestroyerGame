61b5cee5528fcf5e79bf870d4d3a015f649a18a2
Licho <lichobaron@gmail.com>
2019-02-10 23:44:40 -0500

    basic game

---
+++ 2DBricks/Assets/ballMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMove : MonoBehaviour {
+
+	public float force;
+	public Transform ballChecker;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2(0,-2.34f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 0.5f) * Time.deltaTime * force);
+		}
+
+		if (transform.position.y < ballChecker.transform.position.y)
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
---
+++ 2DBricks/Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	Vector2 position;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveX = Input.GetAxis("Horizontal");
+		position.x += moveX * Time.deltaTime  * speed;
+		position.x = Mathf.Clamp(position.x, -5.87f, 5.87f);
+		transform.position = position;			
+	}
+}
