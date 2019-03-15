a5489d7f44d4dd9fef6fc2fd9e472e804ed178c9
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-03-14 22:17:51 -0500

    Archivos de muestreo

--- 2DBricks/Assets/Scripts/BallMovement.cs
+++ 2DBricks/Assets/Scripts/BallMovement.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class BallMovement : MonoBehaviour
 {
@@ -12,6 +19,7 @@ public class BallMovement : MonoBehaviour
     bool startGame = false;
     float xRotation = 5.0f;
     public LineRenderer line;
+    StreamWriter sw;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +27,8 @@ public class BallMovement : MonoBehaviour
         line = GetComponent<LineRenderer>();
         line.SetPosition(1, new Vector3(2, 0));
         line.transform.Rotate(0, 0, 90);
+        sw = new StreamWriter("C:\\Users\\anfec\\Documents\\GitHub\\KMeansHand\\muestreo.txt");
+
     }
 
     // Update is called once per frame
@@ -64,6 +74,43 @@ public class BallMovement : MonoBehaviour
             startGame = true;
             line.enabled = false;
         }
+        double sen1 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131",0);
+        double sen2 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 1);
+        double sen3 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 2);
+        double sen4 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 3);
+        double sen5 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 4);
+        double sen6 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 5);
+        double sen7 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 6);
+        double sen8 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 7);
+        double sen9 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 8);
+        double sen10 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 9);
+        double sen11 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 10);
+        double sen12 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 11);
+        double sen13 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 12);
+        double sen14 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 13);
+
+        try
+        {
+            sw.WriteLine(Math.Round(sen1,3)+", "+ Math.Round(sen2, 3)+", "+Math.Round(sen3, 3) + ", "
+                + Math.Round(sen4, 3) + ", " + Math.Round(sen5, 3) + ", " + Math.Round(sen6, 3) + ", "
+                + Math.Round(sen7, 3) + ", " + Math.Round(sen8, 3) + ", " + Math.Round(sen9, 3) + ", "
+                + Math.Round(sen10, 3) + ", " + Math.Round(sen11, 3) + ", " + Math.Round(sen12, 3) + ", "
+                + Math.Round(sen13, 3) + ", " + Math.Round(sen14, 3));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        sw.Close();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -77,3 +124,5 @@ public class BallMovement : MonoBehaviour
         }
     }
 }
+
+
