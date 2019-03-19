0ae7edb9da9720fd7fb55171eddae786544db808
licho <lichobaron@gmail.com>
2019-03-18 21:09:19 -0500

    intg guante

--- 2DBricks/Assets/Scripts/BallMovement.cs
+++ 2DBricks/Assets/Scripts/BallMovement.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
+using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
@@ -19,16 +15,33 @@ public class BallMovement : MonoBehaviour
     bool startGame = false;
     float xRotation = 5.0f;
     public LineRenderer line;
-    StreamWriter sw;
+
+    private string path = "/Users/licho/Downloads/";
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+
+    private string[] filenames;
+    private double[][][] means;
+    private int[] info;
+    private double[] tuple;
+       
+
     // Start is called before the first frame update
     void Start()
     {
-		audiosource = GetComponent<AudioSource>();
+        filenames = new string[5];
+        //TODO
+        filenames[0] = path + "finger1.txt";
+        filenames[1] = path + "finger2.txt";
+        filenames[2] = path + "finger3.txt";
+        filenames[3] = path + "finger4.txt";
+        filenames[4] = path + "finger5.txt";
+        tuple = new double[14];
+        audiosource = GetComponent<AudioSource>();
         line = GetComponent<LineRenderer>();
         line.SetPosition(1, new Vector3(2, 0));
         line.transform.Rotate(0, 0, 90);
-        sw = new StreamWriter("C:\\Users\\anfec\\Documents\\GitHub\\KMeansHand\\muestreo.txt");
-
     }
 
     // Update is called once per frame
@@ -74,43 +87,49 @@ public class BallMovement : MonoBehaviour
             startGame = true;
             line.enabled = false;
         }
-        double sen1 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131",0);
-        double sen2 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 1);
-        double sen3 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 2);
-        double sen4 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 3);
-        double sen5 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 4);
-        double sen6 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 5);
-        double sen7 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 6);
-        double sen8 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 7);
-        double sen9 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 8);
-        double sen10 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 9);
-        double sen11 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 10);
-        double sen12 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 11);
-        double sen13 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 12);
-        double sen14 = VRPN.vrpnAnalog("Glove14Right@10.3.136.131", 13);
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            tuple[0] = VRPN.vrpnAnalog("Glove14Right@localhost", 0);
+            tuple[1] = VRPN.vrpnAnalog("Glove14Right@localhost", 1);
+            tuple[2] = VRPN.vrpnAnalog("Glove14Right@localhost", 2);
+            tuple[3] = VRPN.vrpnAnalog("Glove14Right@localhost", 3);
+            tuple[4] = VRPN.vrpnAnalog("Glove14Right@localhost", 4);
+            tuple[5] = VRPN.vrpnAnalog("Glove14Right@localhost", 5);
+            tuple[6] = VRPN.vrpnAnalog("Glove14Right@localhost", 6);
+            tuple[7] = VRPN.vrpnAnalog("Glove14Right@localhost", 7);
+            tuple[8] = VRPN.vrpnAnalog("Glove14Right@localhost", 8);
+            tuple[9] = VRPN.vrpnAnalog("Glove14Right@localhost", 9);
+            tuple[10] = VRPN.vrpnAnalog("Glove14Right@localhost", 10);
+            tuple[11] = VRPN.vrpnAnalog("Glove14Right@localhost", 11);
+            tuple[12] = VRPN.vrpnAnalog("Glove14Right@localhost", 12);
+            tuple[13] = VRPN.vrpnAnalog("Glove14Right@localhost", 13);
 
-        try
-        {
-            sw.WriteLine(Math.Round(sen1,3)+", "+ Math.Round(sen2, 3)+", "+Math.Round(sen3, 3) + ", "
-                + Math.Round(sen4, 3) + ", " + Math.Round(sen5, 3) + ", " + Math.Round(sen6, 3) + ", "
-                + Math.Round(sen7, 3) + ", " + Math.Round(sen8, 3) + ", " + Math.Round(sen9, 3) + ", "
-                + Math.Round(sen10, 3) + ", " + Math.Round(sen11, 3) + ", " + Math.Round(sen12, 3) + ", "
-                + Math.Round(sen13, 3) + ", " + Math.Round(sen14, 3));
+            //Debug.Log(Math.Round(sen14,3));
+            means = getMeansFromFile(filenames, 2); //TODO
+            info = TestFingers(tuple, means);
+
+            //Debug.Log(tuple[6]);
+            //Debug.Log(tuple[7]);
+            /*Debug.Log("Glove raw = " + String.Join(", ",
+                new List<double>(tuple)
+                .ConvertAll(i => i.ToString())
+                .ToArray()));*/
+
+            Debug.Log("Glove = " + String.Join(", ",
+                new List<int>(info)
+                .ConvertAll(i => i.ToString())
+                .ToArray()));
         }
-        catch (Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
-        }
-        finally
-        {
-            Console.WriteLine("Executing finally block.");
-        }
+
+    
+
+
 
     }
 
     private void OnApplicationQuit()
     {
-        sw.Close();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -123,6 +142,138 @@ public class BallMovement : MonoBehaviour
             line.enabled = true;
         }
     }
+
+    public static double[] getFingersTuple(double[] tuple, int finger)
+    {
+        double[] r = new double[2];
+        switch (finger)
+        {
+            case 1:
+                r[0] = tuple[0];
+                r[1] = tuple[1];
+                break;
+            case 2:
+                r[0] = tuple[3];
+                r[1] = tuple[4];
+                break;
+            case 3:
+                r[0] = tuple[6];
+                r[1] = tuple[7];
+                break;
+            case 4:
+                r[0] = tuple[9];
+                r[1] = tuple[10];
+                break;
+            case 5:
+                r[0] = tuple[12];
+                r[1] = tuple[13];
+                break;
+            default:
+                r[0] = -1;
+                r[1] = -1;
+                break;
+        }
+        return r;
+    }
+
+    public static double[][][] getMeansFromFile(string[] fileNames, int numK)
+    {
+        double[][][] means = new double[5][][];
+
+        for (int i = 0; i < fileNames.Length; i++)
+        {
+            means[i] = new double[numK][];
+            string filename = fileNames[i];
+            String line; try
+            {
+                StreamReader sr = new StreamReader(filename);
+                line = sr.ReadLine();
+
+                int j = 0;
+                while (line != null)
+                {
+                    Regex reg = new Regex(@"([-+]?[0-9]*\.?[0-9]+)");
+                    int tam = 0;
+                    foreach (Match match in reg.Matches(line))
+                    {
+                        tam++;
+                    }
+                    means[i][j] = new double[tam];
+
+                    int k = 0;
+                    foreach (Match match in reg.Matches(line))
+                    {
+                        means[i][j][k] = double.Parse(match.Value, CultureInfo.InvariantCulture.NumberFormat);
+                        k++;
+                    }
+
+                    j++;
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block files.");
+            }
+        }
+
+        return means;
+    }
+
+    public static int[] TestFingers(double[] tuple, double[][][] means)
+    {
+
+        int[] r = new int[5];
+
+        for (int i = 0; i < 5; i++) //num dedos
+        {
+            int minIndex = 0;
+            double min = Distance(getFingersTuple(tuple, i), means[i][0]);
+            for (int j = 1; j < means[i].Length; j++)
+            {
+                if (Distance(getFingersTuple(tuple, i), means[i][j]) < min)
+                {
+                    minIndex = j;
+                }
+            }
+            r[i] = translateSensor(minIndex);
+        }
+        return r;
+    }
+
+    public static int translateSensor(int num)
+    {
+        //Debug.Log(num);
+        int r = 99;
+        if (num == 0)
+        {
+            r = 0;
+        }
+        else if (num == 1) //TODO
+        {
+            r = 1;
+        }
+        else if (num == 2)
+        {
+            r = -1;
+        }
+        return r;
+    }
+
+    private static double Distance(double[] tuple, double[] mean)
+    {
+        double sumSquaredDiffs = 0.0;
+        for (int j = 0; j < tuple.Length; ++j)
+            sumSquaredDiffs += Math.Pow((tuple[j] - mean[j]), 2);
+        return Math.Sqrt(sumSquaredDiffs);
+
+    }
+
 }
 
 
