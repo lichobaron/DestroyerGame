dd5621745840c7adfccc410ea736e32540f914cd
licho <lichobaron@gmail.com>
2019-03-19 19:30:07 -0500

    left glove

--- 2DBricks/Assets/Scripts/BallMovement.cs
+++ 2DBricks/Assets/Scripts/BallMovement.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-	public AudioClip audioclip;
-	public AudioSource audiosource;
+    public AudioClip audioclip;
+    public AudioSource audiosource;
     public float velX = 0;
     public float velY = 200;
     public Rigidbody2D rb;
@@ -16,7 +16,7 @@ public class BallMovement : MonoBehaviour
     float xRotation = 5.0f;
     public LineRenderer line;
 
-    private string path = "/Users/licho/Downloads/";
+    private string path = "/Users/licho/Documents/varios/KMeansHand/KmeansHand/Data/";
     private float nextActionTime = 0.0f;
     public float period = 0.1f;
 
@@ -25,19 +25,34 @@ public class BallMovement : MonoBehaviour
     private double[][][] means;
     private int[] info;
     private double[] tuple;
-       
+
+    private string[] filenamesl;
+    private double[][][] meansl;
+    private int[] infol;
+    private double[] tuplel;
+
 
     // Start is called before the first frame update
     void Start()
     {
         filenames = new string[5];
         //TODO
-        filenames[0] = path + "finger1.txt";
-        filenames[1] = path + "finger2.txt";
-        filenames[2] = path + "finger3.txt";
-        filenames[3] = path + "finger4.txt";
-        filenames[4] = path + "finger5.txt";
+        filenames[0] = path + "finger13.txt";
+        filenames[1] = path + "finger23.txt";
+        filenames[2] = path + "finger33.txt";
+        filenames[3] = path + "finger43.txt";
+        filenames[4] = path + "finger53.txt";
+
+        filenamesl = new string[5];
+        filenamesl[0] = path + "finger13l.txt";
+        filenamesl[1] = path + "finger23l.txt";
+        filenamesl[2] = path + "finger33l.txt";
+        filenamesl[3] = path + "finger43l.txt";
+        filenamesl[4] = path + "finger53l.txt";
+
         tuple = new double[14];
+        tuplel = new double[14];
+
         audiosource = GetComponent<AudioSource>();
         line = GetComponent<LineRenderer>();
         line.SetPosition(1, new Vector3(2, 0));
@@ -47,9 +62,9 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int zAngle = (int )transform.localEulerAngles.z;        
+        int zAngle = (int)transform.localEulerAngles.z;
         if (Input.GetKey(KeyCode.RightArrow))
-        {   
+        {
             line.transform.Rotate(0, 0, -xRotation);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -58,8 +73,8 @@ public class BallMovement : MonoBehaviour
         }
         if (Input.GetKeyUp(KeyCode.Space) && startGame == false)
         {
-			audiosource.Play();
-            if ( zAngle > 180)
+            audiosource.Play();
+            if (zAngle > 180)
             {
                 velY = -300;
             }
@@ -90,6 +105,37 @@ public class BallMovement : MonoBehaviour
         if (Time.time > nextActionTime)
         {
             nextActionTime += period;
+            tuplel[0] = VRPN.vrpnAnalog("Glove14Left@localhost", 0);
+            tuplel[1] = VRPN.vrpnAnalog("Glove14Left@localhost", 1);
+            tuplel[2] = VRPN.vrpnAnalog("Glove14Left@localhost", 2);
+            tuplel[3] = VRPN.vrpnAnalog("Glove14Left@localhost", 3);
+            tuplel[4] = VRPN.vrpnAnalog("Glove14Left@localhost", 4);
+            tuplel[5] = VRPN.vrpnAnalog("Glove14Left@localhost", 5);
+            tuplel[6] = VRPN.vrpnAnalog("Glove14Left@localhost", 6);
+            tuplel[7] = VRPN.vrpnAnalog("Glove14Left@localhost", 7);
+            tuplel[8] = VRPN.vrpnAnalog("Glove14Left@localhost", 8);
+            tuplel[9] = VRPN.vrpnAnalog("Glove14Left@localhost", 9);
+            tuplel[10] = VRPN.vrpnAnalog("Glove14Left@localhost", 10);
+            tuplel[11] = VRPN.vrpnAnalog("Glove14Left@localhost", 11);
+            tuplel[12] = VRPN.vrpnAnalog("Glove14Left@localhost", 12);
+            tuplel[13] = VRPN.vrpnAnalog("Glove14Left@localhost", 13);
+
+            //Debug.Log(Math.Round(sen14,3));
+            meansl = GetMeansFromFile(filenamesl, 2); //TODO
+            infol = TestFingersLeft(tuplel, meansl);
+
+            //Debug.Log(tuple[6]);
+            //Debug.Log(tuple[7]);
+            /*Debug.Log("Glove raw = " + String.Join(", ",
+                new List<double>(tuple)
+                .ConvertAll(i => i.ToString())
+                .ToArray()));*/
+
+            Debug.Log("GloveL = " + String.Join(", ",
+                new List<int>(infol)
+                .ConvertAll(i => i.ToString())
+                .ToArray()));
+
             tuple[0] = VRPN.vrpnAnalog("Glove14Right@localhost", 0);
             tuple[1] = VRPN.vrpnAnalog("Glove14Right@localhost", 1);
             tuple[2] = VRPN.vrpnAnalog("Glove14Right@localhost", 2);
@@ -106,8 +152,8 @@ public class BallMovement : MonoBehaviour
             tuple[13] = VRPN.vrpnAnalog("Glove14Right@localhost", 13);
 
             //Debug.Log(Math.Round(sen14,3));
-            means = getMeansFromFile(filenames, 2); //TODO
-            info = TestFingers(tuple, means);
+            means = GetMeansFromFile(filenames, 2); //TODO
+            info = TestFingersLeft(tuple, means); //TODO
 
             //Debug.Log(tuple[6]);
             //Debug.Log(tuple[7]);
@@ -116,20 +162,16 @@ public class BallMovement : MonoBehaviour
                 .ConvertAll(i => i.ToString())
                 .ToArray()));*/
 
-            Debug.Log("Glove = " + String.Join(", ",
+            Debug.Log("GloveR = " + String.Join(", ",
                 new List<int>(info)
                 .ConvertAll(i => i.ToString())
                 .ToArray()));
         }
 
-    
 
 
 
-    }
 
-    private void OnApplicationQuit()
-    {
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -143,9 +185,10 @@ public class BallMovement : MonoBehaviour
         }
     }
 
-    public static double[] getFingersTuple(double[] tuple, int finger)
+    public static double[] GetFingersTupleRight(double[] tuple, int finger)
     {
         double[] r = new double[2];
+        finger++;
         switch (finger)
         {
             case 1:
@@ -176,7 +219,41 @@ public class BallMovement : MonoBehaviour
         return r;
     }
 
-    public static double[][][] getMeansFromFile(string[] fileNames, int numK)
+    public static double[] GetFingersTupleLeft(double[] tuple, int finger)
+    {
+        double[] r = new double[2];
+        finger++;
+        switch (finger)
+        {
+            case 1:
+                r[0] = tuple[1];
+                r[1] = tuple[0];
+                break;
+            case 2:
+                r[0] = tuple[4];
+                r[1] = tuple[3];
+                break;
+            case 3:
+                r[0] = tuple[7];
+                r[1] = tuple[6];
+                break;
+            case 4:
+                r[0] = tuple[10];
+                r[1] = tuple[9];
+                break;
+            case 5:
+                r[0] = tuple[13];
+                r[1] = tuple[12];
+                break;
+            default:
+                r[0] = -1;
+                r[1] = -1;
+                break;
+        }
+        return r;
+    }
+
+    public static double[][][] GetMeansFromFile(string[] fileNames, int numK)
     {
         double[][][] means = new double[5][][];
 
@@ -225,7 +302,7 @@ public class BallMovement : MonoBehaviour
         return means;
     }
 
-    public static int[] TestFingers(double[] tuple, double[][][] means)
+    public static int[] TestFingersRight(double[] tuple, double[][][] means)
     {
 
         int[] r = new int[5];
@@ -233,20 +310,78 @@ public class BallMovement : MonoBehaviour
         for (int i = 0; i < 5; i++) //num dedos
         {
             int minIndex = 0;
-            double min = Distance(getFingersTuple(tuple, i), means[i][0]);
+            double min = Distance(GetFingersTupleRight(tuple, i), means[i][0]);
             for (int j = 1; j < means[i].Length; j++)
             {
-                if (Distance(getFingersTuple(tuple, i), means[i][j]) < min)
+                if (Distance(GetFingersTupleRight(tuple, i), means[i][j]) < min)
                 {
                     minIndex = j;
                 }
             }
-            r[i] = translateSensor(minIndex);
+            r[i] = TranslateSensorRight(minIndex, i);
         }
         return r;
     }
 
-    public static int translateSensor(int num)
+    public static int[] TestFingersLeft(double[] tuple, double[][][] means)
+    {
+
+        int[] r = new int[5];
+
+        for (int i = 0; i < 5; i++) //num dedos
+        {
+            int minIndex = 0;
+            double min = Distance(GetFingersTupleLeft(tuple, i), means[i][0]);
+            for (int j = 1; j < means[i].Length; j++)
+            {
+                if (Distance(GetFingersTupleLeft(tuple, i), means[i][j]) < min)
+                {
+                    minIndex = j;
+                }
+            }
+            r[i] = TranslateSensorLeft(minIndex, i);
+        }
+        return r;
+    }
+
+    public static int TranslateSensorRight(int num, int finger)
+    {
+        //Debug.Log(num);
+        int r = 99;
+        if (finger == 2 || finger == 1)
+        {
+            if (num == 0)
+            {
+                r = 1;
+            }
+            else if (num == 1) //TODO
+            {
+                r = 0;
+            }
+            else if (num == 2)
+            {
+                r = -1;
+            }
+        }
+        else
+        {
+            if (num == 0)
+            {
+                r = 0;
+            }
+            else if (num == 1) //TODO
+            {
+                r = 1;
+            }
+            else if (num == 2)
+            {
+                r = -1;
+            }
+        }
+        return r;
+    }
+
+    public static int TranslateSensorLeft(int num, int finger)
     {
         //Debug.Log(num);
         int r = 99;
